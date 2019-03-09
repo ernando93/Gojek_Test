ddaa23dbdf203ac19a350d4f269639f184f89e9b
Ernando <ernandokasaluhe93@gmail.com>
2019-03-09 13:25:00 +0700

    Initial Commit

---
+++ Gojek_TestTests/Gojek_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  Gojek_TestTests.swift
+//  Gojek_TestTests
+//
+//  Created by Ernando Kasaluhe on 09/03/19.
+//  Copyright © 2019 PT Kita Indonesia Plus. All rights reserved.
+//
+
+import XCTest
+@testable import Gojek_Test
+
+class Gojek_TestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
